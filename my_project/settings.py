ebec7007c24945d189068f0118e2c5594893af65
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-03-11 14:32:54 +0000

    add allowed host

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$qp1hn19v25!6t*p&&c5yznb70v(g&95(^0gi=m7^ly41e6!xn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost' ,'.herokuapp.com']
-
+ALLOWED_HOSTS = ['herokuapp.com',  '127.0.0.1']
 
 # Application definition
 
